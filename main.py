07aa24ec3afc7614b710398ae550b69df15566af
anem1c <lshsean9123@gmail.com>
2024-12-01 02:03:26 +0900

    이미지 테스트 완료

--- main.py
+++ main.py
@@ -337,16 +337,29 @@ if uploaded_file is not None:
         # 사용자 메시지 표시
         with st.chat_message("user"):  
             st.write("위 이미지에 대해서 설명해줘.")
-            # AI 응답 처리
+        st.image(image, caption="감지된 결과", use_container_width=True)
+        # AI 응답 처리
         with st.chat_message("assistant"):
-            st.image(image, caption="감지된 결과", use_container_width=True)
+            
             # 이미지에 대한 파손 부위 분석을 위한 프롬프트 생성
             prompt = image_prompt_template.format_messages(content=image_query)
             # AI 모델에 대한 응답 요청 (ChatOpenAI 모델 사용)
             response = translate_model.invoke(prompt)
-            
-            # 응답을 화면에 표시
-            st.write(response.content)
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages = [
+                    *[
+                        {"role": "system", "content":response.content}
+                    ],
+                ],
+                stream = True,
+            )
+            response = st.write_stream(stream)
+            data = {"role":"assistant", "content":response}
+            st.session_state.messages.append(data)
+            session_save(data)
+
+
 
         
 
